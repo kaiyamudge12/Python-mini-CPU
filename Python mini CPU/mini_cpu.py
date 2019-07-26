d4eefe5784c9121abb8d2c2df00cfdca4629ca21
Kaiya Mudge <kaiyamudge12@gmail.com>
2019-07-26 03:17:15 -0500

    first commit

---
+++ Python mini CPU/mini_cpu.py
@@ -0,0 +1,32 @@
+registers = [0,0,0,0,0,0]
+pc = 0
+
+
+program = open("testminicpu.txt","r").readlines()
+while pc < len(program):
+	line = program[pc]
+	print(line)
+	instruct = line[:3]
+	dest = int(line[4])
+	if instruct == 'ADD':
+		registers[dest] = int(line[6]) + int(line[8])
+		pc += 1
+	elif instruct == 'SUB':
+		registers[dest] = int(line[6]) - int(line[8])
+		pc += 1
+	elif instruct == 'JMP':
+		pc = dest
+	elif instruct == 'LDA':
+		registers[int(line[6])] = registers[dest]
+		pc += 1
+	elif instruct == 'IFL':
+		if registers[int(line[6])] < registers[int(line[8])]:
+			pc = dest
+		else:
+			pc += 1
+	elif instruct == "LDV":
+		registers[dest] = int(line[6])
+		pc += 1
+	else:
+		raise ValueError, 'Invalid Instruction ' + line
+	print(registers)
